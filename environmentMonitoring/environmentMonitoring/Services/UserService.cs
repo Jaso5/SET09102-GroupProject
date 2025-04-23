8ef6983ebe0190d4287a6bedb5bc8a108bafedb5
James Anderson <68205198+JamesA89@users.noreply.github.com>
2025-04-23 14:58:35 +0100

    updated log in

--- environmentMonitoring/environmentMonitoring/Services/UserService.cs
+++ environmentMonitoring/environmentMonitoring/Services/UserService.cs
@@ -1,13 +1,14 @@
-using System;
 using environmentMonitoring.Database.Models;
 using environmentMonitoring.Database.Data;
 using Microsoft.EntityFrameworkCore;
 using BCrypt.Net;
 
 
- using System.Diagnostics;
-
 namespace environmentMonitoring.Services;
+
+/*! UserService is responsible for handling user specific services, from validation 
+     *  to retrieving and updating users information.
+     */
 
 public class UserService: IReadDataService, IUpdateDataService, IValidationService
 {
@@ -18,23 +19,61 @@ public class UserService: IReadDataService, IUpdateDataService, IValidationServi
     {
         _context = context;
     }
+
+    /*! GetUserById method retrieves a single user by ID
+     *  @param Takes a user ID as a parameter
+     *  @throws Exception if there is an issue during retrieval
+     *  @return Returns the user 
+     */
+    public User GetUserById(int userId) {
+        try {
+            return _context.Users.Single(u => u.user_Id == userId);
+        } catch (Exception) {
+            throw new Exception("Error retrieving user");
+        }
+    }
+
+    /*! UpdateUserRole method updates a user role
+     *  @param Takes a user object as a parameter
+     *  @throws Exception if there is an issue during the update
+     */
+    public void UpdateUserRole(User user)
+    {
+        try {
+            _context.Users.Update(user);
+            _context.SaveChanges();
+        } catch (Exception) {
+            throw new Exception("Error updating users role");
+        }
+    }
+
+
+    /*! GetUserList method retrieves a list of all users
+     *  @throws Exception if there is an issue during retrieval
+     *  @return Returns a list of all users
+     */
+    public List<User> GetUserList()
+    {
+        try {
+        return _context.Users
+            .Include(u => u.Role)
+            .ToList();
+        } catch (Exception) {
+            throw new Exception("Error retrieving user list");
+        }
+    }
+
+
     public async Task<User?> CredentialsCheck(String email, string password)
     {
          var user = await _context.Users
         .Include(u => u.Role)
-        .ThenInclude(r => r.RolePermissions)
-        .ThenInclude(rp => rp.Permissions)
         .FirstOrDefaultAsync(u => u.email == email);
 
-        bool passwordVerified = BCrypt.Net.BCrypt.Verify(password, user.password);
-
-         if (user != null && passwordVerified) 
+         if (user != null && !BCrypt.Net.BCrypt.Verify(password, user.password)) 
          {
-
             return user;
-            
          }
-
          return null;
         
     }
