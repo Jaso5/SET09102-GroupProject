9fd0c75cab7785dbf9703976f163ffc918a0a0a8
James Anderson <68205198+JamesA89@users.noreply.github.com>
2025-05-02 09:46:55 +0100

    Removed duplicate queries

--- environmentMonitoring/environmentMonitoring/Services/UserService.cs
+++ environmentMonitoring/environmentMonitoring/Services/UserService.cs
@@ -67,52 +67,6 @@ public class UserService: IReadDataService, IUpdateDataService, IValidationServi
         }
     }
 
-
-
-    /*! GetUserById method retrieves a single user by ID
-     *  @param Takes a user ID as a parameter
-     *  @throws Exception if there is an issue during retrieval
-     *  @return Returns the user 
-     */
-    public User GetUserById(int userId) {
-        try {
-            return _context.Users.Single(u => u.user_Id == userId);
-        } catch (Exception) {
-            throw new Exception("Error retrieving user");
-        }
-    }
-
-    /*! UpdateUserRole method updates a user role
-     *  @param Takes a user object as a parameter
-     *  @throws Exception if there is an issue during the update
-     */
-    public void UpdateUserRole(User user)
-    {
-        try {
-            _context.Users.Update(user);
-            _context.SaveChanges();
-        } catch (Exception) {
-            throw new Exception("Error updating users role");
-        }
-    }
-
-
-    /*! GetUserList method retrieves a list of all users
-     *  @throws Exception if there is an issue during retrieval
-     *  @return Returns a list of all users
-     */
-    public List<User> GetUserList()
-    {
-        try {
-        return _context.Users
-            .Include(u => u.Role)
-            .ToList();
-        } catch (Exception) {
-            throw new Exception("Error retrieving user list");
-        }
-    }
-
-
     public async Task<User?> CredentialsCheck(String email, string password)
     {
          var user = await _context.Users
