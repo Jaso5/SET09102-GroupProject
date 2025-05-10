e593ea06de157def4e7e4d7de054f5d6cbdf0b93
James Anderson <68205198+JamesA89@users.noreply.github.com>
2025-05-10 18:41:46 +0100

    Pushing to pull dev

--- environmentMonitoring/environmentMonitoring/ViewModels/SensorViewModel.cs
+++ environmentMonitoring/environmentMonitoring/ViewModels/SensorViewModel.cs
@@ -1,13 +1,17 @@
 using CommunityToolkit.Mvvm.Input;
 using environmentMonitoring.Database.Models;
+using Microsoft.IdentityModel.Tokens;
 using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace environmentMonitoring.ViewModels;
 
-public partial class SensorViewModel
+public partial class SensorViewModel:ObservableObject
 {
     public ICommand BackCommand { get; }
     public RealSensor? rs { get; set; }
+
+    public int? sensorId => rs.r_sensor_Id;
 
     public SensorViewModel()
     {
@@ -16,4 +20,23 @@ public partial class SensorViewModel
 
     [RelayCommand]
     private async Task NavBack() => await Shell.Current.GoToAsync("..");
+
+
+     /*! RefreshProperties refreshes the information in the report when called 
+     */
+     [RelayCommand]
+     private async Task RefreshProperties()
+    {
+        OnPropertyChanged(nameof(sensorId));
+        /*
+        OnPropertyChanged(nameof(reportDateTime));
+        OnPropertyChanged(nameof(incidentType));
+        OnPropertyChanged(nameof(incidentStatus));
+        OnPropertyChanged(nameof(reportLastUpdateDateTime));
+        OnPropertyChanged(nameof(incidentDescription));
+        OnPropertyChanged(nameof(incidentNextSteps));
+        OnPropertyChanged(nameof(incidentResolution));
+        */
+    }
+
 }
